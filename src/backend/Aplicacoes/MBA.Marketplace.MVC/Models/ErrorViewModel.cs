df5fe8b9b62f677472f12169a2d5a0f3fb0f756a
Igor Vettorello <igor.vettorello@hotmail.com>
2025-06-17 22:28:05 -0300

    Commit inicial

---
+++ src/backend/Aplicacoes/MBA.Marketplace.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MBA.DevXpert.Marketplace.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

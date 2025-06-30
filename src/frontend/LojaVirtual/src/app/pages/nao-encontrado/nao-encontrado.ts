a253073fa86e739bbcae5a8877548e9d8bc04810
= <=>
2025-06-30 19:30:51 -0300

    Ajuste na navegação da pagina "not found"

--- src/frontend/LojaVirtual/src/app/pages/nao-encontrado/nao-encontrado.ts
+++ src/frontend/LojaVirtual/src/app/pages/nao-encontrado/nao-encontrado.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nao-encontrado',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nao-encontrado.html'
 })
 export class NaoEncontrado {

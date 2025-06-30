63e22355aefc15fa2c219fe746821835eae04f20
= <=>
2025-06-30 19:10:09 -0300

    Criação do componente de itens em destaque com dados mockados

--- src/frontend/LojaVirtual/src/app/pages/home/home.ts
+++ src/frontend/LojaVirtual/src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Carrossel } from "./carrossel/carrossel";
+import { Destaque } from "./destaque/destaque";
 
 @Component({
   selector: 'app-home',
-  imports: [Carrossel],
+  imports: [Carrossel, Destaque],
   templateUrl: './home.html'
 })
 export class Home {

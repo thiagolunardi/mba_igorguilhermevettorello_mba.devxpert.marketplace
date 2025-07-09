35a53e8e428e7a20c625883fb2278969b026c977
Gustavo Palinka Scabuzzi <gustavo.scabuzzi@gmail.com>
2025-07-08 22:52:31 -0300

    Novos itens add angular favoritos

--- src/frontend/LojaVirtual/src/app/app.routes.ts
+++ src/frontend/LojaVirtual/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './pages/public/home/home';
 import { NaoEncontrado } from './pages/public/nao-encontrado/nao-encontrado';
 import { ProdutoComponent } from './pages/public/produto/produto';
 import { VendedorComponent } from './pages/public/vendedor/vendedor';
+import { Favoritos } from './pages/public/favoritos/favoritos';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'produto/:id', component: ProdutoComponent },
-  { path: 'Vendedor', component: VendedorComponent },
+  { path: 'vendedor', component: VendedorComponent },
+  { path: 'favoritos', component: Favoritos },
   { path: '**', component: NaoEncontrado }
 ];

88ba8ae90f158466b94f7726cbdac25eef5f172a
Cleber Roberto Movio <crmmvio@gmail.com>
2025-07-10 22:00:26 -0300

    Revert "Feature/gustavo/items"

--- src/backend/Aplicacoes/MBA.Marketplace.API/Configurations/DependencyInjectionConfig.cs
+++ src/backend/Aplicacoes/MBA.Marketplace.API/Configurations/DependencyInjectionConfig.cs
@@ -38,7 +38,6 @@ namespace MBA.Marketplace.API.Configurations
             service.AddScoped<IProdutoRepository, ProdutoRepository>();
             service.AddScoped<IVendedorRepository, VendedorRepository>();
             service.AddScoped<IUserRepository<IdentityUser>, UserRepository>();
-            service.AddScoped<IFavoritoRepository, FavoritoRepository>();
         }
 
         private static void RegisterServices(IServiceCollection service)
@@ -47,7 +46,6 @@ namespace MBA.Marketplace.API.Configurations
             service.AddScoped<IProdutoService, ProdutoService>();
             service.AddScoped<IVendedorService, VendedorService>();
             service.AddScoped<IAccountService, AccountService>();
-            service.AddScoped<IFavoritoService, FavoritoService>();
 
         }
     }
--- src/frontend/LojaVirtual/src/app/app.routes.ts
+++ src/frontend/LojaVirtual/src/app/app.routes.ts
@@ -4,8 +4,6 @@ import { NaoEncontrado } from './pages/public/nao-encontrado/nao-encontrado';
 import { ProdutoComponent } from './pages/public/produto/produto';
 import { VendedorComponent } from './pages/public/vendedor/vendedor';
 import { Favoritos } from './pages/public/favoritos/favoritos';
-import { Login } from './pages/public/autenticacao/login/login';
-import { Register } from './pages/public/autenticacao/register/register';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -13,7 +11,5 @@ export const routes: Routes = [
   { path: 'produto/:id', component: ProdutoComponent },
   { path: 'vendedor', component: VendedorComponent },
   { path: 'favoritos', component: Favoritos },
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
   { path: '**', component: NaoEncontrado }
 ];

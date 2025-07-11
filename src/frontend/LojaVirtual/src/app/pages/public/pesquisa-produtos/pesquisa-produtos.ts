4c551986beeacdb644117b20cda852627d953d1a
= <=>
2025-07-11 10:43:37 -0300

    Alteração no controle de erro, retorno com e sem resultado na pesquisa de produtos.

--- src/frontend/LojaVirtual/src/app/pages/public/pesquisa-produtos/pesquisa-produtos.ts
+++ src/frontend/LojaVirtual/src/app/pages/public/pesquisa-produtos/pesquisa-produtos.ts
@@ -1,30 +1,45 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ProdutosService } from '../../../services/produtos.service';
-import { AsyncPipe } from '@angular/common';
 import { NgbProgressbar } from '@ng-bootstrap/ng-bootstrap';
 import { ResumoProduto } from "./resumo-produto/resumo-produto";
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
 import { ProdutoViewModel } from '../../../viewmodels/pesquisa-de-produtos/produto.viewmodel';
 import { ListaPaginada } from '../../../viewmodels/shared/lista-paginada.viewmodel';
 
 @Component({
   selector: 'app-pesquisa-produtos',
-  imports: [AsyncPipe, NgbProgressbar, ResumoProduto],
+  imports: [NgbProgressbar, ResumoProduto],
   templateUrl: './pesquisa-produtos.html',
   styles: ``
 })
 export class PesquisaProdutos implements OnInit {
   private produtoService = inject(ProdutosService);
   private activatedRoute = inject(ActivatedRoute);
-
-  produtos$!: Observable<ListaPaginada<ProdutoViewModel> | null>;
+  carregando: boolean = true;
+  erro: boolean = false;
+  listaDeProdutosPaginada?: ListaPaginada<ProdutoViewModel> | null;
 
   ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe(params => {
       const termo = params['termo'];
 
-      this.produtos$ = this.produtoService.obterProdutos(termo);
+      this.obterProdutos(termo);
     });
   }
+
+  obterProdutos(termo: string | null = null) {
+    this.produtoService.obterProdutos(termo).subscribe({
+      next: (resposta) => {
+        this.listaDeProdutosPaginada = resposta;
+        this.erro = false;
+      },
+      error: (err) => {
+        this.erro = true;
+      },
+      complete: () => {
+        this.carregando = false;
+      }
+    });
+  }
+
 }

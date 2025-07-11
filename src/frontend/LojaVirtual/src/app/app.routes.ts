77e1a368cb11fb62303cbcceccc490bacdd0ddb5
= <=>
2025-07-10 22:30:15 -0300

    Inclusao da rota de detalhes do produto

--- src/frontend/LojaVirtual/src/app/app.routes.ts
+++ src/frontend/LojaVirtual/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { NaoEncontrado } from './pages/public/nao-encontrado/nao-encontrado';
 import { Favoritos } from './pages/public/favoritos/favoritos';
 import { Login } from './pages/public/autenticacao/login/login';
 import { Register } from './pages/public/autenticacao/register/register';
+import { ProdutoComponent } from './pages/public/produto/produto';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'favoritos', component: Favoritos },
+  { path: 'produto/:id', component: ProdutoComponent },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: '**', component: NaoEncontrado }

a30e8b056827a9f5222c6268ef17b0cc6096f742
= <=>
2025-07-21 22:36:05 -0300

    Reestruturação das paginas no Angular: criação do layout main (com menu, barra de pesquisa, footer, etc) e auth layout (sem itens adicionais)

--- src/frontend/LojaVirtual/src/app/layout/main-layout/main-layout.ts
+++ src/frontend/LojaVirtual/src/app/layout/main-layout/main-layout.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from '../header/header';
-import { Footer } from '../footer/footer';
-import { Breadcrumb } from '../breadcrumb/breadcrumb';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 import { Notificacao } from '../notificacao/notificacao';
+import { Breadcrumb } from './breadcrumb/breadcrumb';
 
 
 @Component({

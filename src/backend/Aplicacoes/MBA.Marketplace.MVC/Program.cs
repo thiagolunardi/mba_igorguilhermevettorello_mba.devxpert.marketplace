290b6ca74825604e9514b3fe453e7e03fb903bef
Igor Vettorello <igor.vettorello@hotmail.com>
2025-07-28 23:36:52 -0300

    feat: ajuste dos modais de retorno de mensagens do cadastro de categoria

--- src/backend/Aplicacoes/MBA.Marketplace.MVC/Program.cs
+++ src/backend/Aplicacoes/MBA.Marketplace.MVC/Program.cs
@@ -13,7 +13,6 @@ builder.Services.AddLoggingConfig(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");

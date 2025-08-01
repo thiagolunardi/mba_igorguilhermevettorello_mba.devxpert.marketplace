66c94a861f9c146057796e415edce232bc97d768
= <=>
2025-08-01 12:59:16 -0300

    SPA - Adicionado link para a pagina do vendedor na tela de detalhe do produto

--- src/frontend/LojaVirtual/src/app/pages/public/produto-detalhes/components/info-produto/info-produto.ts
+++ src/frontend/LojaVirtual/src/app/pages/public/produto-detalhes/components/info-produto/info-produto.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { ProdutoViewModel } from '../../../../../services/produto.viewmodel';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-info-produto', // O seletor foi mantido por ser descritivo
   standalone: true,
-  imports: [CommonModule, CurrencyPipe],
+  imports: [CommonModule, CurrencyPipe, RouterLink],
   templateUrl: './info-produto.html', // Caminho do template atualizado
   styleUrls: ['./info-produto.scss']
 })

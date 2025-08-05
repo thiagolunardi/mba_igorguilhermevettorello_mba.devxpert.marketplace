be7933310f35e00177da0d314640432cb6ba4b88
Gustavo Palinka Scabuzzi <gustavo.scabuzzi@gmail.com>
2025-08-04 23:25:29 -0300

    Implementado endpoint de produto com filtros de limite e ordenacao; Retirado endpoint de mktplace; Ajustes gerais home SPA

--- src/frontend/LojaVirtual/src/app/pages/public/home/item-destaque/item-destaque.ts
+++ src/frontend/LojaVirtual/src/app/pages/public/home/item-destaque/item-destaque.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ItemEmDestaqueViewModel } from '../item-destaque/item-em-destaque.viewmodel';
+import { IMAGEM_PLACEHOLDER } from '../../../../util/constantes';
 
 @Component({
   selector: 'app-item-destaque',
@@ -12,4 +13,14 @@ import { ItemEmDestaqueViewModel } from '../item-destaque/item-em-destaque.viewm
 })
 export class ItemDestaqueComponent {
   @Input() item!: ItemEmDestaqueViewModel;
+
+    get imagemSrc(): string {
+
+    if (this.item?.imagem) {
+      return `data:image/jpeg;base64,${this.item.imagem}`;
+    }
+
+    return IMAGEM_PLACEHOLDER;
+  }
+  
 }

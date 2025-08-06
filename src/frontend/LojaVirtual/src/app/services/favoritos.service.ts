2407eca594fabc8b06861e444dcdf239adfebb37
Rafael Gimenes <gimenes_rafael_f1@cat.com>
2025-08-05 22:19:14 -0300

    ajustes6

--- src/frontend/LojaVirtual/src/app/services/favoritos.service.ts
+++ src/frontend/LojaVirtual/src/app/services/favoritos.service.ts
@@ -1,9 +1,9 @@
-import { inject, Injectable } from "@angular/core";
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { adicionarParametrosSePossuirValor } from "../util/common-functions";
-import { ListaPaginada } from "../viewmodels/shared/lista-paginada.viewmodel";
-import { ProdutoViewModel } from "../viewmodels/pesquisa-de-produtos/produto.viewmodel";
-import { catchError, map, of } from "rxjs";
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { adicionarParametrosSePossuirValor } from '../util/common-functions';
+import { ListaPaginada } from '../viewmodels/shared/lista-paginada.viewmodel';
+import { ProdutoViewModel } from '../viewmodels/pesquisa-de-produtos/produto.viewmodel';
+import { catchError, map, of } from 'rxjs';
 
 export interface Favorito {
   id: number;
@@ -15,10 +15,9 @@ export interface Favorito {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FavoritosService {
-
   private http = inject(HttpClient);
 
   public obterProdutos(
@@ -32,38 +31,53 @@ export class FavoritosService {
     let params = new HttpParams();
 
     //adicionar parâmetros ao request somente se eles tiverem valor
-    params = adicionarParametrosSePossuirValor(
-      params,
-      [
-        { nome: 'termoPesquisado', valor: termoPesquisado },
-        { nome: 'categoriaId', valor: categoriaId },
-        { nome: 'numeroDaPagina', valor: numeroDaPagina },
-        { nome: 'tamanhoDaPagina', valor: tamanhoDaPagina },
-        { nome: 'orderBy', valor: orderBy }
-      ]);
+    params = adicionarParametrosSePossuirValor(params, [
+      { nome: 'termoPesquisado', valor: termoPesquisado },
+      { nome: 'categoriaId', valor: categoriaId },
+      { nome: 'numeroDaPagina', valor: numeroDaPagina },
+      { nome: 'tamanhoDaPagina', valor: tamanhoDaPagina },
+      { nome: 'orderBy', valor: orderBy },
+    ]);
 
-    return this.http.get<ListaPaginada<ProdutoViewModel>>(url, { params, observe: 'response' })
-        .pipe(
-          map(response => {
-            if (response.status === 200) {
-              return response.body;
-            }
-            else {
-              throw new Error(`Erro ao buscar produtos. Status: ${response.status}`);
-            }
-          }),
-          catchError(() => of(null)) // retorna null em caso de erro
-        );
+    return this.http
+      .get<ListaPaginada<ProdutoViewModel>>(url, {
+        params,
+        observe: 'response',
+      })
+      .pipe(
+        map((response) => {
+          if (response.status === 200) {
+            return response.body;
+          } else {
+            throw new Error(
+              `Erro ao buscar produtos. Status: ${response.status}`
+            );
+          }
+        }),
+        catchError(() => of(null)) // retorna null em caso de erro
+      );
+  }
 
+  // adicionarFavorito(produto: any) {
+  //   if (!this.favoritos.find(item => item.id === produto.id)) {
+  //     this.favoritos.push(produto);
+  //   }
+  // }
 
-    // adicionarFavorito(produto: any) {
-    //   if (!this.favoritos.find(item => item.id === produto.id)) {
-    //     this.favoritos.push(produto);
-    //   }
-    // }
+  removerFavorito(id: string) {
+    let url = 'https://localhost:7179/api/favoritos/' + id;
 
-    // removerFavorito(produto: any) {
-    //    this.favoritos = this.favoritos.filter(item => item.id !== produto.id);
-    // }
+    return this.http.delete<any>(url, { observe: 'response' }).pipe(
+      map((response) => {
+        if (response.status === 204) {
+          return response.body;
+        } else {
+          throw new Error(
+            `Erro ao buscar produtos. Status: ${response.status}`
+          );
+        }
+      }),
+      catchError(() => of(null)) // retorna null em caso de erro
+    );
   }
 }

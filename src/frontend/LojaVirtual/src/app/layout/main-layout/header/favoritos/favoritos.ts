bd17c89784fdc28e850f25297dc7c3396685efcf
= <=>
2025-08-07 21:05:29 -0300

    Ajustes no html do header

--- src/frontend/LojaVirtual/src/app/layout/main-layout/header/favoritos/favoritos.ts
+++ src/frontend/LojaVirtual/src/app/layout/main-layout/header/favoritos/favoritos.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favoritos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './favoritos.html',
   styleUrls: ['./favoritos.scss']
 })
--- src/frontend/LojaVirtual/src/app/layout/main-layout/header/pesquisa/pesquisa.ts
+++ src/frontend/LojaVirtual/src/app/layout/main-layout/header/pesquisa/pesquisa.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pesquisa',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './pesquisa.html',
   styleUrls: ['./pesquisa.scss'],
   standalone: true
